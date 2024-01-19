79df9e88807ac0b96b181fdc7c594d45e5744cdc
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2024-01-19 18:32:18 +0300

    feat: release chat ws

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './app.css'
+import LongPulling from "./LongPulling";
+import EventSourcing from "./EventSourcing";
+import WebSock from "./WebSock";
+
+function App() {
+
+  return (
+      <div>
+        {/*<LongPulling/>*/}
+        {/*  <WebSock/>*/}
+          <EventSourcing/>
+      </div>
+  )
+}
+
+
+export default App;
